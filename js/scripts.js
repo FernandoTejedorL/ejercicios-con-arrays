ef09727366f4b578e268411c1ad96cacbce22db4
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-10-22 23:01:16 +0200

    completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,142 @@
-/*Crea una función que reciba un array de 5 números, 
+/* 1. Crea una función que reciba un array de 5 números, 
 imprime por consola una posición aleatoria del array.*/
 
-function randomNumber(numbers) {
-    const randomPosition = Math.floor(Math.random() * 5);
-    console.log(numbers[randomPosition])
+const randomNumber = (numbers) => {
+  console.log(numbers[Math.floor(Math.random() * numbers.length)]);
 }
+randomNumber([5, 15, 20, 25, 30]);
 
-const numbers = [5, 15, 20, 25, 30];
-randomNumber(numbers)
-
-/*Crea una función que reciba un array con 3 números.
+/* 2. Crea una función que reciba un array con 3 números. 
 La función deberá imprimir por consola.
+
   - "La suma de todos los números es:[suma]"
   - "La media de todos los números es:[media]"
-  - "El mayor es [mayor] y el menor es [menor]" */
+  - "El mayor es [mayor] y el menor es [menor]"*/
 
-function sumAveTop(numbers2) {
-    const sum = numbers2[0] + numbers2[1] + numbers2[2] + numbers2[3] + numbers2[4];
-    const average = (numbers2[0] + numbers2[1] + numbers2[2] + numbers2[3] + numbers2[4]) / 5;
-    console.log(`La suma de todos los números es ${sum}`);
-    console.log(`La media de todos los números es ${average}`);
-
+const topOfThree = (numbersA) => {
+if (numbersA[0] > numbersA[1] && numbersA[0] > numbersA[2]) {
+    return numbersA[0];
+  } else if (numbersA[1]>numbersA[0] && numbersA[1]>numbersA[2]){
+    return numbersA[1];
+  } else if (numbersA[2]>numbersA[0] && numbersA[2]>numbersA[1]) {
+    return numbersA[2];
+  }
+}
+const botOfThree = (numbersA) => {
+  if (numbersA[0] < numbersA[1] && numbersA[0] < numbersA[2]) {
+    return numbersA[0];
+  } else if (numbersA[1]<numbersA[0] && numbersA[1]<numbersA[2]){
+    return numbersA[1];
+  } else if (numbersA[2]<numbersA[0] && numbersA[2]<numbersA[1]) {
+    return numbersA[2];
+  }
+}
+const threeAbout = (numbersA) => {
+  const sumOfThree = numbersA[0] + numbersA[1] + numbersA[2];
+  const aveOfThree = (numbersA[0] + numbersA[1] + numbersA[2]) / 3;
+  console.log('La suma de todos los números es: ' + sumOfThree);
+  console.log('La media de todos los números es: ' + aveOfThree);
+  console.log(`El mayor es ${topOfThree(numbersA)} y el menor es ${botOfThree(numbersA)}`)
 }
 
-const numbers2 = [ 10 , 20 , 30]
-sumAveTop(numbers2)  
+threeAbout([12, 15, 16])
+
+/* 3. Crea una función que reciba un array con 5 números del 0 al 10 (a tu 
+elección). Dentro de la función genera un número aleario entre 0 y 10. 
+La función deberá decir si el array contiene ese número y en qué 
+posición está o si no lo contiene.*/
+
+const outOfTen = (numbersB) => {
+  const position1 = numbersB[0];
+  const position2 = numbersB[1];
+  const position3 = numbersB[2];
+  const position4 = numbersB[3];
+  const position5 = numbersB[4];
+  const randomPosition = Math.floor(Math.random() * 11);
+  if (numbersB.includes(randomPosition)){
+    return 'El array contiene el número ' + randomPosition +
+      ' en la posición ' + numbersB.indexOf(randomPosition);
+  } else {
+    return 'El array no contiene el número ' + randomPosition
+}
+}
+const result3 = outOfTen([2, 4, 6, 8, 9]);
+console.log(result3);
+
+/* 4. Crea una función que reciba un array vacío y lo devuelva 
+con 3 números aleatorios entre 0 y 100.*/
+
+const createArray = (array) => {
+    const firstComp = Math.floor(Math.random() * 101);
+  const secondComp = Math.floor(Math.random() * 101);
+  const thirdComp = Math.floor(Math.random() * 101);
+  return [firstComp,secondComp,thirdComp];
+}
+const result4 = createArray([]);
+console.log(result4);
+ 
+
+/* 5. Crea una función llamada dniLetter que recibirá un número de DNI
+ sin la letra. Dentro de esa función pon este array 
+ ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 
+ 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'] 
+ La letra del DNI se calcula a través del resto de dividir 
+ el número de DNI entre 23, ese número te dará la posición 
+ del array donde se encuentra la letra correspondiente a ese DNI. 
+ Imprime por consola el DNI con su letra correspondiente.*/
+
+ const dniLetter = (dni) => {
+  const position = dni % 23;
+  const letter = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
+    'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+  return dni + letter[position];
+}
+const result5 = dniLetter(50152587);
+console.log(result5);
+ 
+
+ /* 6. Crea una función que reciba un array con 3 palabras, debes imprimir
+ por consola un array que contenga la inicial y la última letra de 
+ cada palabra en mayúsculas, es decir, si nuestra función recibiera 
+ un array con ['hola', 'adios', 'gato'] deberá imprimir por consola 
+ ['H', 'A', 'A', 'S', 'G', 'O']*/
+
+const begAndEnd = (array6) => {
+  const begOne = array6[0].charAt(0).toUpperCase();
+  const endOne = array6[0].charAt(array6[0].length-1).toUpperCase();
+  const begTwo = array6[1].charAt(0).toUpperCase();
+  const endTwo = array6[1].charAt(array6[1].length - 1).toUpperCase();
+  const begThree = array6[2].charAt(0).toUpperCase();
+  const endThree = array6[2].charAt(array6[2].length-1).toUpperCase();
+  return [begOne, endOne, begTwo, endTwo, begThree, endThree];
+}
+const result6 = begAndEnd(['cebolla', 'queso', 'piña']);
+console.log(result6);
+ 
+
+/* 7. Crea una función que reciba un array con 3 números y te diga cuántos 
+números pares tiene ese array.*/
+const isOdd = (arrayNum) => arrayNum % 2 === 0
+const oddCount = (array7) => {
+  const oddFirst = isOdd(array7[0]);
+  const oddSecond = isOdd(array7[1]);
+  const oddThird = isOdd(array7[2]);
+  return oddFirst+oddSecond+oddThird
+}
+const result7 = oddCount([1, 4, 6])
+console.log(result7)
+
+
+/* 8. Crea una función que reciba un array con 3 números y devuelva un 
+nuevo array con los mismos números pero en orden inverso.*/
+
+const reverseNum = (array8) => [array8[2], array8[1], array8[0]];
+const result8 = reverseNum([12, 20, 25])
+console.log(result8)
+
+/* 9. Crea una función que reciba un array con 3 palabras y devuelva un 
+nuevo array con las mismas palabras pero en mayúsculas.*/
+
+const threeToUpper = (array9) => [array9[0].toUpperCase(), array9[1].toUpperCase(), array9[2].toUpperCase()];
+const result9 = threeToUpper(['cama', 'hamaca', 'acordeon'])
+console.log(result9)
